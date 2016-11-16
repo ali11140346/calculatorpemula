7c2fa32450790cc5d715478caaf4ac03e00860bf
almus <almus@alrosul>
2016-11-16 19:33:11 +0700

    11140346 mendeklerasikan variabel inputan berupa angka1 dan angka2 yang digunakan untuk menyimpan data inputan sebelum diproses. jumlah adalah hasil prosesnya. dan mendeklarasikan angka=0 agar di  layar menampilkan angka 0.

--- src/kalkulator.java
+++ src/kalkulator.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author almus
- */
 public class kalkulator extends javax.swing.JFrame {
-
+    String angka;
+    double jumlah,angka1,angka2;
+    int pilihan;
     /**
      * Creates new form kalkulator
      */
     public kalkulator() {
         initComponents();
+        angka="";
     }
 
     /**
